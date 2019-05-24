c2eacc622378b52ab26958e2283363a1b6f6df75
Marion Cointre <marion.cointre@gmail.com>
2019-05-24 13:56:25 -0700

    Test to fix scroll on update

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,14 +1,66 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import Chart from "./Components/Chart";
 import Chart from "./modals/entrepreneurship";
 
 const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  labels: [
+    "2007",
+    "2008",
+    "2009",
+    "2010",
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017"
+  ],
   datasets: [
     {
-      label: "My First dataset",
+      label: "Enrepreneurship",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(75,192,192,1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 300, 81, 56, 55, 40]
+    },
+    {
+      label: "First Jobs",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(75,192,192,1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 300, 81, 56, 55, 40]
+    },
+    {
+      label: "Post Grad",
       fill: false,
       lineTension: 0.1,
       backgroundColor: "rgba(75,192,192,0.4)",
@@ -33,10 +85,64 @@ const data = {
 class App extends React.Component {
   state = {
     data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: [
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017"
+      ],
       datasets: [
         {
-          label: "My First dataset",
+          label: "Enrepreneurship",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [55, 49, 200, 21, 52, 53, 60]
+        },
+        {
+          label: "First Jobs",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [75, 89, 100, 61, 36, 95, 20]
+        },
+        {
+          label: "Post Grad",
           fill: false,
           lineTension: 0.1,
           backgroundColor: "rgba(75,192,192,0.4)",
@@ -60,19 +166,26 @@ class App extends React.Component {
     }
   };
 
-  sliderHandle = event => {
-    let newData = this.state.data;
-    newData.datasets[0].data[2] = event.target.value;
-    this.setState({
-      data: newData
-    });
-  };
+  // sliderHandle = event => {
+  //   let newData = this.state.data;
+  //   newData.datasets[0].data[2] = event.target.value;
+  //   this.setState({
+  //     data: newData
+  //   });
+  // };
 
+  // myfunction(event) {
+  //   let newData = this.state.data;
+  //   newData.datasets[0].data[2] = event.target.value;
+  //   this.setState({
+  //     data: newData
+  //   });
+  // }
   render() {
     return (
       <div className="App">
         <Chart data={this.state.data} />
-        <input type="range" onChange={this.sliderHandle} min={0} max={100} />
+        <input type="range" onChange={this.myfunction} min={0} max={100} />
         {/* <Main /> */}
       </div>
     );
