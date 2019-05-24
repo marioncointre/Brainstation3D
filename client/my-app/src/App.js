78bd78e2095e60195a99009d219d2988a71bc6d2
Marion Cointre <marion.cointre@gmail.com>
2019-05-24 12:55:42 -0700

    linechart

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import Chart from "./Components/Chart";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        {/* <Main /> */}
+        <p>Test</p>
+        {/* <Header />*/}
+        <Chart />
       </div>
     );
   }
---
+++ client/my-app/src/Components/Chart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+class Chart extends React.Component {
+  render() {
+    const data = {
+      labels: ["2007", "2008", "2009", "2010", "2011", "2012", "2013"],
+      datasets: [
+        {
+          label: "Views",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: "Views",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [6, 5, 8, 8, 5, 5, 4]
+        }
+      ]
+    };
+
+    return (
+      <div>
+        <h2>Line Example</h2>
+        <Line data={data} />
+      </div>
+    );
+  }
+}
+
+export default Chart;
